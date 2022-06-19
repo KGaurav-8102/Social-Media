736d3f0445fb9e2754f35c77e6b028ce82a694b9
Gaurav Yadav <gk5480325@gmail.com>
2022-06-19 20:00:18 +0530

    FollowersData for followers card done

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from '../img/img1.png';
+import img2 from '../img/img2.png';
+import img3 from '../img/img3.png';
+import img4 from '../img/img4.jpg';
+
+export const Followers = [
+    {name: 'Andrew Thomas', username: 'andrewThomas', img:img1},
+    {name: 'Hulk Buster', username: 'hulkBuster', img:img2},
+    {name: 'Thor', username: 'thunderMaster', img: img3},
+    {name:'Natasha', username: 'natasha', img: img4}
+];
