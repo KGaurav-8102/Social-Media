e76d7a3656f99782920b2d9f927916a308fc6d46
Gaurav Yadav <gk5480325@gmail.com>
2022-06-19 20:03:21 +0530

    Followers Card Done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import Home from './Pages/home/Home';
 import "./App.css"
+
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeeps code
+      <div className="blur" style={{top: '-18%', right: '0'}}></div>
+      <div className="blur" style={{ top: '36%', left: '-8rem'}}></div>
+      <Home />
     </div>
   );
 }
