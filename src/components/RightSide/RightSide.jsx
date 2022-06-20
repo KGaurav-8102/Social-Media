4ccc2835cd1420c433c4a48f7ac8baca393c7430
Gaurav Yadav <gk5480325@gmail.com>
2022-06-20 16:24:44 +0530

    RightSide has Done

---
+++ src/components/RightSide/RightSide.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Home from '../../img/home.png';
+import Noti from '../../img/noti.png';
+import Comment from '../../img/comment.png';
+import { UilSetting } from '@iconscout/react-unicons';
+import TrendCard from '../TrendCard/TrendCard';
+import './RightSide.css';
+
+const RightSide = () => {
+    return (
+       <div className="RightSide">
+            <div className="navIcons">
+                <img src={Home} alt="" />
+                <UilSetting />
+                <img src={Noti} alt="" />
+                <img src={Comment} alt="" />
+            </div>
+            <TrendCard />
+            <button className='button r-button'>
+                     Share
+            </button>
+       </div>
+    );
+}
+
+export default RightSide;
