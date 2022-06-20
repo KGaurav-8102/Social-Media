361fc97a165d459dd7a6b527855da9986da07355
Gaurav Yadav <gk5480325@gmail.com>
2022-06-20 16:25:37 +0530

    PostShare has done

---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Posts.css';
+import Post from '../Post/Post';
+import { PostsData } from '../../Data/PostsData';
+
+const Posts = () => {
+    return (
+        <div className="Posts">
+            {PostsData.map((post, id) => {
+                return <Post data={post} id={id} />
+            })}
+        </div>
+    );
+}
+
+export default Posts;
