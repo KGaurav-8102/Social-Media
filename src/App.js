9907ec631b6318b9a980d92835fe3ff4123a8262
Gaurav Yadav <gk5480325@gmail.com>
2022-06-21 10:55:51 +0530

    Frontend has Completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Home from './Pages/home/Home';
+import Profile from './Pages/Profile/Profile';
+import Auth from './Pages/Auth/Auth';
 import "./App.css"
 
 function App() {
@@ -7,7 +9,9 @@ function App() {
     <div className="App">
       <div className="blur" style={{top: '-18%', right: '0'}}></div>
       <div className="blur" style={{ top: '36%', left: '-8rem'}}></div>
-      <Home />
+      {/* <Home /> */}
+       <Profile />
+      {/* <Auth /> */}
     </div>
   );
 }
--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Home from '../../img/home.png';
 import Noti from '../../img/noti.png';
 import Comment from '../../img/comment.png';
 import { UilSetting } from '@iconscout/react-unicons';
 import TrendCard from '../TrendCard/TrendCard';
+import ShareModel from '../ShareModel/ShareModel';
 import './RightSide.css';
 
 const RightSide = () => {
+    const [modelOpened, setModelOpened] = useState(false);
     return (
        <div className="RightSide">
             <div className="navIcons">
@@ -16,9 +18,10 @@ const RightSide = () => {
                 <img src={Comment} alt="" />
             </div>
             <TrendCard />
-            <button className='button r-button'>
-                     Share
+            <button className='button r-button' onClick={() => setModelOpened(true)}>
+                    Share
             </button>
+            <ShareModel modelOpened={modelOpened} setModelOpened={setModelOpened} />
        </div>
     );
 }
